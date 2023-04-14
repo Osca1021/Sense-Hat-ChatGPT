d80af97cc911354e705e3e976749075036a896ff
Osca1021 <104490439+Osca1021@users.noreply.github.com>
2023-04-14 10:43:47 +0200

    Start integrating sense hat

--- Main.py
+++ Main.py
@@ -1,16 +1,26 @@
 import openai
+from sense_emu import SenseHat
 openai.api_key_path = "API-Key.txt"
 
 #sends a prompt to chatgpt
 def get_answer(prompt):
 	if (prompt_is_not_safe(prompt)):
 		return
-	return openai.Completion.create(model="text-davinci-002", prompt=prompt, max_tokens=10)
+	messages=[
+		{"role": "system", "content": "You are a helpful assistent. Answer as concisely as possible."},
+		{"role": "user", "content": prompt}
+	]
+	return openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages, max_tokens=50)
 
 def prompt_is_not_safe(prompt):
 	response = openai.Moderation.create(input=prompt)
 	return response["results"][0]["flagged"]
 
+sense = SenseHat()
+
 userPrompt = input("> ")
 answer = get_answer(userPrompt)
+responseMessage = answer['choices'][0]['message']
 print(answer)
+
+sense.show_message(responseMessage['content'])
